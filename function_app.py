3cdae2a77c0847be3903352303a9bc3c2da93770
gino885 <ginozxcv885@gmail.com>
2024-03-01 12:05:11 +0800

    upload latest podcasts

---
+++ function_app.py
@@ -0,0 +1,91 @@
+import logging
+import os
+import azure.functions as func
+import requests
+from azure.storage.blob import BlobServiceClient, BlobBlock
+import feedparser
+import uuid
+import base64
+from dateutil import parser
+
+app = func.FunctionApp()
+def upload_rss_entity_to_blob(connection_string, container_name, blob_name, podcast_data):
+    try:
+        blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+        container_client = blob_service_client.get_container_client(container_name)
+        blob_client = container_client.get_blob_client(blob=blob_name)
+        # 分 chunk 上傳的
+        chunk_size = 4 * 1024 * 1024  # 4MB
+        block_list = []
+        size = len(podcast_data)
+        start = 0
+        number = 0
+
+        while start < size:
+            end = min(start + chunk_size, size)
+            chunk_data = podcast_data[start:end]
+            block_id = str(uuid.uuid4()).replace('-', '')
+            encoded_block_id = base64.b64encode(block_id.encode()).decode()
+
+            blob_client.stage_block(block_id=encoded_block_id, data=chunk_data)
+            block_list.append(BlobBlock(block_id=encoded_block_id))
+
+            start += chunk_size
+
+            # 提交所有 chunks 以完成上传
+        blob_client.commit_block_list(block_list)
+        print(f"Successfully uploaded '{blob_name}' to Azure Blob Storage.")
+           
+
+    except Exception as e:
+        print(f"Failed to upload '{blob_name}' to Azure Blob Storage. Error: {e}")
+
+rss_feeds = [
+    {"url": "https://feeds.megaphone.fm/allearsenglish", "prefix": "【All Ears English Podcast】"},
+    {"url": "https://feeds.buzzsprout.com/1974862.rss", "prefix": "【百靈果】"}
+]
+
+@app.schedule(schedule="0 0 2 * * *", arg_name="myTimer", run_on_startup=True, use_monitor=False)
+def timer_trigger(myTimer: func.TimerRequest) -> None:
+    if myTimer.past_due:
+        logging.info('The timer is past due!')
+
+    # Azure Blob Storage 配置
+    connection_string = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
+    container_name = os.getenv("AZURE_STORAGE_CONTAINER_NAME")
+    
+    # 初始化 Azure Blob Storage 客户端
+    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+    container_client = blob_service_client.get_container_client(container_name)
+
+    # 處理最新的 Podcast
+    for feed_info in rss_feeds:
+        rss_url = feed_info["url"]
+        prefix = feed_info["prefix"]
+
+        feed = feedparser.parse(rss_url)
+        if feed.entries:
+            entry = feed.entries[0]  # 取最新的一集
+            podcast_url = entry.links[0].href
+        
+            file_name = podcast_url.split("/")[-1]
+            if 'enclosures' in entry and len(entry.enclosures) > 0:
+                podcast_url = entry.enclosures[0]['href']  # 使用 enclosure中的URL
+                blob_name= f"{prefix}[{parser.parse(entry.published).strftime('%Y%m%d')}] {entry.title}.mp3"  
+            else: 
+                publish_date = parser.parse(entry.published)  # 使用 dateutil解析發布日期
+                formatted_date = publish_date.strftime('%Y%m%d')
+                blob_name = f"{prefix}[{formatted_date}] {file_name}"
+            
+            
+        # 上傳到 Azure Blob Storage
+            try:
+                response = requests.get(podcast_url)
+                if response.status_code == 200:
+                    podcast_data = response.content
+                upload_rss_entity_to_blob(connection_string, container_name, blob_name, podcast_data)
+                logging.info(f"Uploaded {blob_name} to Azure Blob Storage.")
+            except Exception as e:
+                logging.error(f"Failed to upload {blob_name}. Error: {e}")
+
+    logging.info('Python timer trigger function executed.')
