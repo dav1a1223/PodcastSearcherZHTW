575e782a9bbf770b3a71def09fb2c9b2f925ce15
gino885 <ginozxcv885@gmail.com>
2024-03-04 09:30:54 +0800

    upload 40 episodes

--- function_app.py
+++ function_app.py
@@ -6,9 +6,29 @@ from azure.storage.blob import BlobServiceClient, BlobBlock
 import feedparser
 import uuid
 import base64
+import json
 from dateutil import parser
+from datetime import datetime
+
+
 
 app = func.FunctionApp()
+
+def get_downloaded_status(blob_service_client, container_name):
+    try:
+        container_client = blob_service_client.get_container_client(container_name)
+        blob_client = container_client.get_blob_client(blob="downloaded_episodes_status.json")
+        download_status_json = blob_client.download_blob().readall()
+        download_status = json.loads(download_status_json)
+    except Exception as e:
+        download_status = {}
+    return download_status
+
+def update_downloaded_status(blob_service_client, container_name, download_status):
+    container_client = blob_service_client.get_container_client(container_name)
+    blob_client = container_client.get_blob_client(blob="downloaded_episodes_status.json")
+    blob_client.upload_blob(json.dumps(download_status), overwrite=True)
+
 def upload_rss_entity_to_blob(connection_string, container_name, blob_name, podcast_data):
     try:
         blob_service_client = BlobServiceClient.from_connection_string(connection_string)
@@ -41,8 +61,7 @@ def upload_rss_entity_to_blob(connection_string, container_name, blob_name, podc
         print(f"Failed to upload '{blob_name}' to Azure Blob Storage. Error: {e}")
 
 rss_feeds = [
-    {"url": "https://feeds.megaphone.fm/allearsenglish", "prefix": "【All Ears English Podcast】"},
-    {"url": "https://feeds.buzzsprout.com/1974862.rss", "prefix": "【百靈果】"}
+    {"url": "https://feeds.soundon.fm/podcasts/adf29720-e93b-4856-a09e-b73544147ec4.xml", "prefix": "【好味小姐】"}
 ]
 
 @app.schedule(schedule="0 0 2 * * *", arg_name="myTimer", run_on_startup=True, use_monitor=False)
@@ -53,19 +72,27 @@ def timer_trigger(myTimer: func.TimerRequest) -> None:
     # Azure Blob Storage 配置
     connection_string = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
     container_name = os.getenv("AZURE_STORAGE_CONTAINER_NAME")
-    
+
+
     # 初始化 Azure Blob Storage 客户端
     blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-    container_client = blob_service_client.get_container_client(container_name)
+    download_status = get_downloaded_status(blob_service_client, container_name)
 
     # 處理最新的 Podcast
     for feed_info in rss_feeds:
         rss_url = feed_info["url"]
         prefix = feed_info["prefix"]
-
         feed = feedparser.parse(rss_url)
-        if feed.entries:
-            entry = feed.entries[0]  # 取最新的一集
+        episodes_to_download = []
+    
+        if rss_url not in download_status:  # 第一次下載這
+            episodes_to_download = feed.entries[:40]  # 下載最後40集
+        else:
+            latest_episode_guid = feed.entries[0].get("guid")
+            if download_status[rss_url]["guid"] != latest_episode_guid:    
+                episodes_to_download = feed.entries[0]
+
+        for entry in episodes_to_download: #依次下載
             podcast_url = entry.links[0].href
         
             file_name = podcast_url.split("/")[-1]
@@ -76,15 +103,24 @@ def timer_trigger(myTimer: func.TimerRequest) -> None:
                 publish_date = parser.parse(entry.published)  # 使用 dateutil 解析發布日期
                 formatted_date = publish_date.strftime('%Y%m%d')
                 blob_name = f"{prefix}[{formatted_date}] {file_name}"
-            
+            latest_episode_guid = entry.get("guid", None)
             
         # 上傳到 Azure Blob Storage
             try:
-                response = requests.get(podcast_url)
-                if response.status_code == 200:
-                    podcast_data = response.content
-                upload_rss_entity_to_blob(connection_string, container_name, blob_name, podcast_data)
-                logging.info(f"Uploaded {blob_name} to Azure Blob Storage.")
+                if(latest_episode_guid != None):
+                    logging.info(f"Downloading {podcast_url} to {blob_name}...")
+                    response = requests.get(podcast_url)
+                    if response.status_code == 200:
+                        podcast_data = response.content
+                    upload_rss_entity_to_blob(connection_string, container_name, blob_name, podcast_data)
+                    download_status[rss_url] = {
+                        "last_downloaded": str(datetime.now()),
+                        "guid": latest_episode_guid
+                    }
+                    update_downloaded_status(blob_service_client, container_name, download_status)
+                    logging.info(f"Uploaded {blob_name} to Azure Blob Storage.")
+                else:
+                    logging.info(f"Skipping {blob_name} because it is not a valid podcast.")
             except Exception as e:
                 logging.error(f"Failed to upload {blob_name}. Error: {e}")
 
