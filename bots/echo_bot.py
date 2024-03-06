4b17fcea4d0c7fc47c286a19fd55b70f121f61cb
hyching21 <sheep128763@gmail.com>
2024-03-07 04:20:26 +0800

    update to original echobot

--- bots/echo_bot.py
+++ bots/echo_bot.py
@@ -3,7 +3,8 @@
 
 from botbuilder.core import ActivityHandler, MessageFactory, TurnContext
 from botbuilder.schema import ChannelAccount
-from linebot.models import TemplateSendMessage, ButtonsTemplate, PostbackTemplateAction
+#from linebot.models import TemplateSendMessage, ButtonsTemplate, PostbackTemplateAction
+
 
 class EchoBot(ActivityHandler):
     async def on_members_added_activity(
@@ -14,33 +15,6 @@ class EchoBot(ActivityHandler):
                 await turn_context.send_activity("Hello and welcome!")
 
     async def on_message_activity(self, turn_context: TurnContext):
-        if turn_context.activity.text=="@button":
-
-            buttons_template = ButtonsTemplate(
-                #thumbnail_image_url='https://example.com/image.jpg',
-                title='My Button Template',
-                text='Please select an option:',
-                actions=[
-                    PostbackTemplateAction(
-                        label='Option 1',
-                        data='action=option1'
-                    ),
-                    PostbackTemplateAction(
-                        label='Option 2',
-                        data='action=option2'
-                    )
-                ]
-            )
-
-            # Create a template message containing the button template
-            template_message = TemplateSendMessage(
-                alt_text='Button Template',
-                template=buttons_template
-            )
-
-            # Send the template message
-            await turn_context.send_activity(template_message)
-        else:   
-            return await turn_context.send_activity(
-                MessageFactory.text(f"Echo: {turn_context.activity.text}")
-            )
+        return await turn_context.send_activity(
+            MessageFactory.text(f"Echo: {turn_context.activity.text}")
+        )
